4370336f7a088c5589ea817eb78565f68c4a2104
anadeso <anadeso90@gmail.com>
2021-10-27 09:15:46 -0300

    Validacao do token via filter nas requisicoes

--- src/main/java/br/com/alura/carteira/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/carteira/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.alura.carteira.infra.security;
 
+import br.com.alura.carteira.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
@@ -20,6 +22,12 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private  TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @Override
     @Bean
@@ -38,12 +46,15 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().csrf().disable();
+                .and().csrf().disable()
+                .addFilterBefore(
+                        new VerificacaoTokenFilter(tokenService, usuarioRepository),
+                                UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v3/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**");
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**");
     }
 
     //    public static void main(String[] args) {
---
+++ src/main/java/br/com/alura/carteira/infra/security/VerificacaoTokenFilter.java
@@ -0,0 +1,50 @@
+package br.com.alura.carteira.infra.security;
+
+import br.com.alura.carteira.entities.Usuario;
+import br.com.alura.carteira.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class VerificacaoTokenFilter extends OncePerRequestFilter {
+
+   private TokenService tokenService;
+   private UsuarioRepository usuarioRepository;
+
+    public VerificacaoTokenFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
+        this.tokenService = tokenService;
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        //recuperar token, validar, caso correto liberar requisicao
+        String token = httpServletRequest.getHeader("Authorization");
+        if (token == null || token.isEmpty()) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            return;
+        }
+
+        token = token.replace("Bearer ", "");
+        boolean tokenValido = tokenService.isValido(token);
+
+        if (tokenValido) {
+            Long idUsuario = tokenService.extrairIdUsuario(token);
+            Usuario logado = usuarioRepository.getById(idUsuario);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(logado, null, null);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
